af7725e9602bec5660eb130bdc465359c23d7a6d
Roman Kovalov <kovalyov2727@gmail.com>
2019-05-13 19:15:37 +0300

    my first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+let money = prompt ('Ваш бюджет на месяц?','');
+let time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+var exprenses;
+var firstQuetion = prompt ('“Введите обязательную статью расходов в этом месяце”','');
+var secondQuetion = prompt ('“Во сколько обойдется?”','');
+
+let appData = {
+    бюджет: money,
+    timeData: time,
+    обязательныеРасходы:  exprenses = {
+        firstQuetion: "ответ на первый вопрос",
+        secondQuetion: "ответ на второй вопрос"
+     },
+     optionalExpenses: "",
+     допДоход: income=[,,,]
+}
+alert (+money/30);
