e5b52813eec9f9e32626931f74320d77cfbcc64e
Roman Kovalov <kovalyov2727@gmail.com>
2019-05-18 15:13:46 +0300

    this is my third commit, i created function

--- js/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-var i=1
-do { 
-        
-        if (i%2==0) {
-        continue
-} else {
-        console.log(i)
-} i++;
-} while (i<10);
---
+++ js/trainie.js
@@ -0,0 +1,11 @@
+
+// function truncate( str, maxlength) {
+//     if (str.length > maxlength) {
+//         return str.slice(0, maxlength-3)+ "..."
+//     }else {
+//         return str;
+//     }
+    
+    
+// }
+// console.log( truncate("Вот, что мне хотелось бы сказать на эту тему:", 10) );
